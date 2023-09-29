f17606256ae9ff13d63cc659888c50abaf91f7a9
amitsinghparihar <amitsinghp1007@gmail.com>
2023-09-29 14:46:26 +0530

    moddifying

--- placement/app.py
+++ placement/app.py
@@ -1,28 +1,36 @@
 import streamlit as st
 import pickle
 
-model=pickle.load(open('svr.pkl','rb'))
+model = pickle.load(open('svr.pkl', 'rb'))
 
 
 def predict(input_data):
+    try:
+        prediction = model.predict(input_data)
+        return prediction
+    except Exception as e:
+        return str(e)
 
-    prediction=model.predict(input_data)
-    return prediction
-    
 
 def main():
-    
     st.title("Placement Package Predictor")
-    cgpa = st.text_input('Enter the cgpa')
     
-    result=[]
+    st.write("Enter your CGPA below and click 'Predict' to estimate your placement package.")
     
-    if st.button('Predict Prediction'):
-         result= predict([[cgpa]])
-         
-    st.success(result)
+    cgpa = st.number_input('Enter your CGPA', min_value=0.0, max_value=10.0, step=0.01)
     
-if __name__=='__main__':
+    result = None
+    
+    if st.button('Predict'):
+        if cgpa is not None:
+            result = predict([[cgpa]])
+    
+    if result is not None:
+        st.success(f"Predicted Package: {result[0]:.2f} lakhs")
+    else:
+        st.warning("Please enter a valid CGPA and click 'Predict'.")
+
+if __name__ == '__main__':
     main()
         
          
